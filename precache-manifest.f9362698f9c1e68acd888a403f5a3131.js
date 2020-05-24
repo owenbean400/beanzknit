c9f8c940a232ab5b748ae8361949f0dc2786ca87
owenbean400 <owenbean400@gmail.com>
2020-05-24 12:22:10 -0400

    Updates

---
+++ precache-manifest.f9362698f9c1e68acd888a403f5a3131.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672178aed3b1fbc8c40c5d7dcc0367e7",
+    "url": "/beanzknit/index.html"
+  },
+  {
+    "revision": "28da8d82110379ce0fd4",
+    "url": "/beanzknit/static/css/main.364a6c14.chunk.css"
+  },
+  {
+    "revision": "b34061753bb15970fc68",
+    "url": "/beanzknit/static/js/2.df425030.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beanzknit/static/js/2.df425030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28da8d82110379ce0fd4",
+    "url": "/beanzknit/static/js/main.5f092da3.chunk.js"
+  },
+  {
+    "revision": "47efa482ff91cb114d66",
+    "url": "/beanzknit/static/js/runtime-main.3381c704.js"
+  },
+  {
+    "revision": "4a09192f1d7551872fdde7e1e33d8c19",
+    "url": "/beanzknit/static/media/bw_blossom_scarf.4a09192f.jpg"
+  },
+  {
+    "revision": "9d327c5ea80ecee03602d2a3cd2adeb2",
+    "url": "/beanzknit/static/media/facemask.9d327c5e.jpg"
+  },
+  {
+    "revision": "4934e1b67dbd664695628fc1f6f4dfdd",
+    "url": "/beanzknit/static/media/linedownhat.4934e1b6.jpg"
+  },
+  {
+    "revision": "cca179b75873312dff9b2afcb06f54a4",
+    "url": "/beanzknit/static/media/mixturehat.cca179b7.jpg"
+  },
+  {
+    "revision": "6c48e5360a3b34d041f1f50af6af184d",
+    "url": "/beanzknit/static/media/owenbeanhat.6c48e536.jpg"
+  },
+  {
+    "revision": "45ac6491a7551393d779d8a8ecd306c1",
+    "url": "/beanzknit/static/media/pinkiescarf.45ac6491.jpg"
+  },
+  {
+    "revision": "bee57415a06e59926881bf27157616ef",
+    "url": "/beanzknit/static/media/queen_of_diamonds_scarf.bee57415.jpg"
+  },
+  {
+    "revision": "d2d87548b53168114b9151b91b382e20",
+    "url": "/beanzknit/static/media/queendiamondstexture.d2d87548.png"
+  },
+  {
+    "revision": "0922d303cb18d582a60306894c4d0676",
+    "url": "/beanzknit/static/media/ribbing_scarf.0922d303.jpg"
+  },
+  {
+    "revision": "3cabd281c01885de1bf160fcc8e0a8d1",
+    "url": "/beanzknit/static/media/splitwayz_scarf.3cabd281.jpg"
+  },
+  {
+    "revision": "d6621fc0d540f6895c390e682cb3c2d7",
+    "url": "/beanzknit/static/media/summer_fade_scarf.d6621fc0.jpeg"
+  },
+  {
+    "revision": "5d9ae0b50dcd6162a1cdf36f61aa9fb2",
+    "url": "/beanzknit/static/media/three_cables_scarf.5d9ae0b5.jpeg"
+  },
+  {
+    "revision": "2697024e338364cfb976f54f307d6c58",
+    "url": "/beanzknit/static/media/wallet.2697024e.jpg"
+  }
+]);
